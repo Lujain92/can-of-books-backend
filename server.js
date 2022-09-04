09dd3d8d8bd05f85e6c9d06578968832a8733023
Lujain92 <lujainaljarrah92@gmail.com>
2022-09-04 21:11:14 +0300

    done

--- server.js
+++ server.js
@@ -8,11 +8,67 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+const bookModel=require("./schema") //scheme 
 
+//seeding data
+async function seedData(){
+const firstBook = new bookModel({
+  title: "dog",
+    description: "doglover",
+    status: "yes",
+
+})
+
+const secondtBook = new bookModel({
+  title: "cat",
+    description: "catlover",
+    status: "yes",
+
+})
+
+const thirdBook = new bookModel({
+  title: "lion",
+    description: "lionlover",
+    status: "yes",
+
+})
+
+
+await firstBook.save();
+await secondtBook.save();
+await thirdBook.save();
+}
+
+//seedData()
+//Route
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
+
+//http://localhost:3001/books
+app.get("/books",bookHandler)
+
+//function
+ function bookHandler(req,res){
+bookModel.find({},(err,result)=>{
+
+
+if(err)
+{
+    console.log(err);
+}
+else
+{
+    console.log(result);
+    res.send(result);
+
+}
+
+}
+)
+
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
