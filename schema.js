c66d3834d19204ca75ee1057cf21ab98c9b806b4
Lujain92 <lujainaljarrah92@gmail.com>
2022-09-06 19:32:56 +0300

    K

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect('mongodb://localhost:27017/books', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://Lujain92:0000@ac-9uywefh-shard-00-00.c3pszlh.mongodb.net:27017,ac-9uywefh-shard-00-01.c3pszlh.mongodb.net:27017,ac-9uywefh-shard-00-02.c3pszlh.mongodb.net:27017/?ssl=true&replicaSet=atlas-tw6af0-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 //mongodb://lujain92:0000@ac-0myqeed-shard-00-00.ahebopu.mongodb.net:27017,ac-0myqeed-shard-00-01.ahebopu.mongodb.net:27017,ac-0myqeed-shard-00-02.ahebopu.mongodb.net:27017/?ssl=true&replicaSet=atlas-9s6est-shard-0&authSource=admin&retryWrites=true&w=majority
 const Book = new mongoose.Schema({
   title: String,
